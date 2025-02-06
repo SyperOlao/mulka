4d164eabbbabc98ac17c5be42702a2747728fccf
syper_olao <70114083+SyperOlao@users.noreply.github.com>
2025-02-06 16:49:53 +0300

    Start dialog manager

---
+++ Assets/Scripts/Dialogs/Data/Character/DialogCharacterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dialogs.Data.Character
+{
+    [Serializable]
+    public class DialogCharacterDto
+    {
+        public string Name { get; set; }
+        public string PngPath { get; set; }
+        public string NameColor { get; set; }
+        public string TextColor { get; set; }
+        public string BgColor { get; set; }
+    }
+}
---
+++ Assets/Scripts/Dialogs/Data/DialogData/DialogDataDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dialogs.Data.SpeechText
+{
+    [Serializable]
+    public class DialogDataDto
+    {
+        public string subject;
+        public Queue<string> text;
+    }
+}
